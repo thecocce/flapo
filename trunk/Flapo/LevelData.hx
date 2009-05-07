681487c01064b4bb8eebdfc28c6defbb103a717a
dobosbence <dobosbence@302f8b70-1929-11de-85e6-8b0190298422>
2009-05-07 15:09:50 +0000

    Levelcontainer tovább bántása. Most már beállítja a kezdő pályát rendesen de még nem az igazi.

--- trunk/Flapo/LevelData.hx
+++ trunk/Flapo/LevelData.hx
@@ -6,24 +6,31 @@
 package flapo;
 
 import flash.display.BitmapData;
-import gamelib2d.TileSet;
-import gamelib2d.Layer;
+//import gamelib2d.TileSet;
+//import gamelib2d.Layer;
+import gamelib2d.MapData;
+import gamelib2d.TileSetData;
 
 class LevelData 
 {
 
 	public var name: String;
-	public var TileSets: Array <TileSet>;
-	public var Layers: Array<Layer>;
+	public var MapDatas: Array<MapData>;
+	public var theBitmapData: TileSetData;
 	
 	public function new() 
 	{
-		TileSets = new Array<TileSet>();
-		Layers = new Array<Layer>();
+		MapDatas = new Array<MapData>();
+		theBitmapData = null;
 	}
 	
-	public function AddTile(tile: Class<BitmapData>)
+	public function AddTile(tile: TileSetData)
 	{
-
+		theBitmapData = tile;
+	}
+	
+	public function AddMap(map: MapData)
+	{
+		MapDatas.push(map);
 	}
 }
